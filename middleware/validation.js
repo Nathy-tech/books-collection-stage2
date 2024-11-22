68a6c7c90a3b8fa4afce42045029a0923775e5b2
Nathy-tech <mannathy5@gmail.com>
2024-11-22 04:39:40 -0800

    setting the validation

--- middleware/validation.js
+++ middleware/validation.js
@@ -1,1 +1,14 @@
+const Joi = require('joi');
+
+const validateBook = (data) => {
+    const schema = Joi.object({
+        title: Joi.string().min(3).required(),
+        author: Joi.string().min(3).required(),
+        isbn: Joi.string().min(10).max(13).required(),
+        publishedYear: Joi.number().integer().min(1000).max(9999).required()
+    });
+    return schema.validate(data);
+};
+
+module.exports = validateBook;
 
