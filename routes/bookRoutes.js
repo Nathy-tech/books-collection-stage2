bc0cb3cc5ad137279d1b0c4d18d67824d4eef624
Nathy-tech <mannathy5@gmail.com>
2024-11-22 04:41:09 -0800

    setting up the routes

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { getBooks, addBook, updateBook, deleteBook } = require('../controllers/bookController');
+
+router.get('/books', getBooks);
+router.post('/books', addBook);
+router.put('/books/:id', updateBook);
+router.delete('/books/:id', deleteBook);
+
+module.exports = router;
 
