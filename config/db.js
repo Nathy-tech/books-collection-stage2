c913c9df0e682e9d80733179ee24b59c0db56893
Nathy-tech <mannathy5@gmail.com>
2024-11-22 04:39:01 -0800

    book model

--- config/db.js
+++ config/db.js
@@ -1,1 +1,14 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/booksdb', { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log('MongoDB Connected');
+    } catch (err) {
+        console.error('MongoDB connection error:', err);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
 
--- models/Book.js
+++ models/Book.js
@@ -1,1 +1,30 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, 'Title is required']
+    },
+    author: {
+        type: String,
+        required: [true, 'Author is required']
+    },
+    isbn: {
+        type: String,
+        required: [true, 'ISBN is required'],
+        unique: true
+    },
+    publishedYear: {
+        type: Number,
+        required: [true, 'Published year is required']
+    },
+    isFavorite: {
+        type: Boolean,
+        default: false
+    }
+});
+
+const Book = mongoose.model('Book', bookSchema);
+
+module.exports = Book;
 
