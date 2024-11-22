0377562a230d4546deb9eb68c65b8f4106c1fb59
Nathy-tech <mannathy5@gmail.com>
2024-11-22 04:41:59 -0800

    setting the server

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const bookRoutes = require('./routes/bookRoutes');
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Connect to Database
+connectDB();
+
+// Routes
+app.use('/api', bookRoutes);
+
+// Start the Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
