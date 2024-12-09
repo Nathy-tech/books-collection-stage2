e4bed9a57f9391516a6f549464b01338c64436ee
Nathy-tech <mannathy5@gmail.com>
2024-12-09 09:44:16 -0800

    middleare import chage usermodel

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/User'); // Import User model
+const User = require('../models/userModel'); // Import User model
 
 // Authentication Middleware
 exports.authenticate = (req, res, next) => {
